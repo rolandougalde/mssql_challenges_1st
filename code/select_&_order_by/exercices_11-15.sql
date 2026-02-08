ea37e85d3665d15d5e34fa43093e775e3f729ca1
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2026-02-07 19:25:49 -0600

    Create SQL exercises for employee and customer queries
    Added SQL exercises for retrieving employee and customer data.

---
+++ code/select_&_order_by/exercices_11-15.sql
@@ -0,0 +1,31 @@
+/*SQL Server Exercise Development*/
+------------------------------------
+
+USE Northwind;
+
+-- 11. Retrieve the employees ordered alphabetically by FirstName and LastName.
+
+SELECT *
+FROM Employees
+ORDER BY FirstName, LastName;
+
+-- 12.Retrieve the employees where the Title column has the value ‘Sales Representatives’ 
+-- and the City field has the values Seattle or Redmond.
+
+
+SELECT *
+FROM Employees
+WHERE Title = 'Sales Representative' 
+	AND City = 'Seattle' OR City = 'Redmond';
+
+-- 13. Retrieve the columns CompanyName, ContactTitle, City, and Country from 
+-- the Customers who are in Mexico City or in any city in Spain except Madrid.
+
+
+-- 14. Retrieve the list of orders, and display a column where a 10% tax is 
+-- calculated when the value of the Freight column is greater than or equal to 480.
+
+
+-- 15. Retrieve the number of employees for each city.
+
+-- END
