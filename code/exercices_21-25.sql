ff1050b84b261e6843660ac06f7102eb4b76a24f
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2026-02-18 14:23:47 -0600

    Add ranking query for salesmen based on total sales
    Added a query to rank salesmen by total sales per year using RANK() with OVER(PARTITION).

--- code/exercices_21-25.sql
+++ code/exercices_21-25.sql
@@ -19,3 +19,17 @@ INNER JOIN Orders T2 ON T2.EmployeeID = T1.EmployeeID
 INNER JOIN [Order Details] T3 ON T3.OrderID = T2.OrderID
 GROUP BY T1.FirstName, T1.LastName, T2.OrderDate
 ORDER BY 4 DESC
+
+-- Solved as ranking using OVER(PARTITION)
+
+SELECT
+	RANK() OVER(PARTITION BY DATEPART(YEAR, T2.OrderDate) ORDER BY SUM(T3.Quantity * T3.UnitPrice) DESC) AS Ranking,
+	T1.FirstName + ' ' + T1.LastName AS [Sales Man],
+	DATEPART(YEAR, T2.OrderDate) AS [Year],
+	COUNT (T2.OrderID) AS [Nro. Orders],
+	SUM(T3.Quantity * T3.UnitPrice) AS [Total]
+FROM Employees T1
+INNER JOIN Orders T2 ON T2.EmployeeID = T1.EmployeeID
+INNER JOIN [Order Details] T3 ON T3.OrderID = T2.OrderID
+GROUP BY T1.FirstName, T1.LastName, T2.OrderDate
+
