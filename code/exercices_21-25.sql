2e67afd5d3a3c6daa596fca5098cb2cebb668bf3
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2026-02-19 17:08:10 -0600

    Add query for total sales by product category
    Added SQL query to show total sales grouped by product category.

--- code/exercices_21-25.sql
+++ code/exercices_21-25.sql
@@ -50,3 +50,19 @@ GROUP BY T1.FirstName, T1.LastName, T2.OrderDate
 ) AS T
 WHERE Ranking < 6
 
+-- 22. Show the total sales grouped by product category.
+
+-- Step 1: get data
+SELECT * FROM Categories
+SELECT * FROM "Order Details"
+SELECT * FROM Products
+
+-- Solved
+
+SELECT
+	T3.CategoryName,
+	SUM(T1.Quantity * T1.UnitPrice) AS [Total sale]
+FROM [Order Details] T1
+INNER JOIN Products T2 ON T2.ProductID = T1.ProductID
+INNER JOIN Categories T3 ON T3.CategoryID = T2.CategoryID
+GROUP BY T3.CategoryName
