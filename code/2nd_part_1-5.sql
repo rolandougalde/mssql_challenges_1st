960055b806bf6854f551b4c7c0994ce3caf94eb2
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2026-02-20 15:37:39 -0600

    Add SQL exercise for retrieving Employees records

---
+++ code/2nd_part_1-5.sql
@@ -0,0 +1,10 @@
+/*SQL SERVER EXERCISE DEVELOPMENT*/
+------------------------------------
+
+USE Northwind;
+
+--1. Show the records from the Employees table.
+
+-- Solved:
+
+SELECT * FROM Employees
