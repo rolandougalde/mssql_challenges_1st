a1b33db77a6a1e0e24d4359e3b0523431d6698f5
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2026-03-05 13:46:20 -0600

    Create SQL file with exercises 14-17
    Added SQL exercises for suppliers, orders, and customers.

---
+++ code/2nd_part_14-17.sql
@@ -0,0 +1,14 @@
+/*SQL SERVER EXERCISES*/
+------------------------------------
+
+--14. Show the suppliers and the number of products they have sold.
+
+--15. Show the number of orders and their valuation (price times quantity),
+--grouped by year. Order by year.
+
+--16. Show the number of unique customers who bought each product category.
+--The example uses a Pivot Table added to the model.
+
+--17. Show the total number of orders and the total amount for each of them.
+
+-- END
