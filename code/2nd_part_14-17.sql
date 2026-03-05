49881aa82a7ed29e5eddef3255c872375f250e61
Rolando Ugalde <46721519+rolandougalde@users.noreply.github.com>
2026-03-05 13:51:40 -0600

    Add query to show suppliers and product quantities

--- code/2nd_part_14-17.sql
+++ code/2nd_part_14-17.sql
@@ -2,6 +2,13 @@
 ------------------------------------
 
 --14. Show the suppliers and the number of products they have sold.
+SELECT
+	S.SupplierID,
+	S.Companyname,
+	COUNT(P.ProductID) AS [Product Quantity]
+FROM Suppliers S
+INNER JOIN Products P ON P.SupplierID = S.SupplierID
+GROUP BY S.SupplierID, S.CompanyName
 
 --15. Show the number of orders and their valuation (price times quantity),
 --grouped by year. Order by year.
